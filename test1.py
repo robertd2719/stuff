2ee6abc4a67a2e6f1f8775dc64d35557b58af699
rdeal <robertd2719@gmail.com>
2020-03-05 23:28:06 -0500

    more commits

--- test1.py
+++ test1.py
@@ -1,10 +1,14 @@
 print('some testing stuff for git')
 
+
 def factorial(value):
-    if value <= 1: return 1
+    if value <= 1:
+        return 1
     else:
         return factorial(value-1) * value
 
-list1 = [factorial(x) for x in range(10)]
-print(list1)
 
+list1 = [factorial(x) for x in range(10)]
+
+print(list1)
+print('hello darkness my old friend')
