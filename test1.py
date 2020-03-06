75570a28de3a2c7fd23347e1033f38cd0b00e5c9
rdeal <robertd2719@gmail.com>
2020-03-05 20:09:27 -0500

    initial setup of all relevant files

---
+++ test1.py
@@ -0,0 +1,10 @@
+print('some testing stuff for git')
+
+def factorial(value):
+    if value <= 1: return 1
+    else:
+        return factorial(value-1) * value
+
+list1 = [factorial(x) for x in range(10)]
+print(list1)
+
